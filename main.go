1403b28c17a717753be617e489894e900f0c1520
Srinivasa Reddy <thisismsreddy@gmail.com>
2017-01-08 01:10:42 +0530

    Added create server api

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
 	opts, err := openstack.AuthOptionsFromEnv()
 	fmt.Println(opts, err)
 	provider, err := openstack.AuthenticatedClient(opts)
@@ -18,8 +17,15 @@ func main() {
 	client, err := openstack.NewComputeV2(provider, gophercloud.EndpointOpts{
 		Region: os.Getenv("OS_REGION_NAME"),
 	})
-	fmt.Println("This line 21")
-	server, err := servers.Get(client, "ad4e58d1-bc68-40f6-8c67-27a71caa33b8").Extract()
-
+	// To create a new server
+	server, err := servers.Create(client, servers.CreateOpts{
+		Name:      "My new server!",
+		FlavorRef: "m1.small",
+		ImageRef:  "ubuntu",
+	}).Extract()
 	fmt.Println("server name is ", server.Name, "server Id", server.ID, server.AccessIPv4, server.Status, server.SecurityGroups)
+
+	//  Get a list of servers
+	//  server, err := servers.Get(client, "server.ID").Extract()
+
 }
